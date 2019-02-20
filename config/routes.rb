7172cd208fc2c3ffda8c000a5cc40244d78f109d
urkazan16 <urkazan16@gmail.com>
2019-02-21 01:22:23 +0300

    NEw page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
